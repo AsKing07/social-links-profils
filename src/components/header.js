658345ec2859b207e3b15b8e677b167a3825db23
Charbel Sonon <charbelsnn@gmail.com>
2024-02-11 18:31:14 +0100

    maked header

---
+++ src/components/header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const head =() => {
+  return (
+    <div className='header'>
+        <div className='image'>
+            <img src='assets/images/avatar-jessica.jpeg' alt='personne' />
+        </div>
+        <div className='identity'>
+            <h1 className='name'>Charbel SONON</h1>
+            <span>Cotonou, Bénin </span>
+            <p className='fontion'>"Full Stack Developer | Web Designer"</p>
+        </div>
+    </div>
+  )
+}
+
+export default head;
