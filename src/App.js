9976463b4ad971e5f67860774ce71b665b9b6f27
Charbel Sonon <charbelsnn@gmail.com>
2024-02-11 17:54:15 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="App" >
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +17,9 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+
+
     </div>
   );
 }
